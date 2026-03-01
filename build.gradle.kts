18f550b33c221c90a728f877a68f821438c32ac7
ImKDO <KDOSAP@protonmail.com>
2026-03-02 00:20:20 +0300

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.0")
+}
+
+kotlin {
+    jvmToolchain(22)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
